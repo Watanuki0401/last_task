3a669be007aa41ec47c140cb467b2d4c280a496e
Watanuki0401 <114453407+Watanuki0401@users.noreply.github.com>
2023-01-28 14:11:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ function hmgbtn(btn) {
   var space = document.createElement('div');
   space.classList = "hoge";
   insert_element.insertBefore(space, hoge);
+
+  let create_element = document.querySelector(".wrapper");
+  var new2_element = document.createElement('div');
+  new2_element.classList = "Humlist";
+  new2_element.insertAdjacentHTML('afterbegin', '<div class="Hs_menu"><li><a href="#Home">Home</a></li><li><a href="#Info">Info</a></li><li><a href="#Hobby">Hobby</a></li><li><a href="#Contact">Contact</a></li></div><div class="Hs_links"><li><a href="https://twitter.com"><i class="fa-brands fa-twitter"></i></a></li><li><a href="https://github.com/Watanuki0401"><i class="fa-brands fa-github"></i></a></li></div>');
+
+  create_element.appendChild(new2_element);
 }
 
 function delbtn() {
@@ -29,9 +36,12 @@ function delbtn() {
   if( target === null) return;
   target.remove();
   document.querySelector(".hoge").remove();
+  document.querySelector(".Humlist").remove();
 }
 
 function hum(){
+  let menu = document.querySelector(".Humlist");
   let btn = document.querySelector(".trigger");
+  menu.classList.toggle('active');
   btn.classList.toggle('active');
 }
